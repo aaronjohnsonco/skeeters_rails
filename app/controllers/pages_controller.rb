5f5dc3bc25c4ddf21ba53e88340606db8e7f8ad3
Aaron Johnson <aaronjohnsonis@gmail.com>
2015-06-19 14:35:47 -0500

    add post to login

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   def home
     @posts = Post.where("date >= ?", Date.today).order('date desc').limit(4)
     @events = Event.where("start_date >= ?", Date.today).limit(4)
+    @users = User.all
   end
 
   def calendar
