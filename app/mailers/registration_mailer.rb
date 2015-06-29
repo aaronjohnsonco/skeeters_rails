f2882a5a58655761c72e9d1de8324d779a35bf94
Aaron Johnson <aaronjohnsonis@gmail.com>
2015-06-29 16:35:23 -0500

    email

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -1,12 +1,12 @@
 class RegistrationMailer < ActionMailer::Base
   def registration_email(candidate)
   	@candidate = candidate
-  	mail(to: 'skeetersballclub@gmail.com', subject: 'Tryout Registration', from: @candidate.contact1_email)
+  	mail(to: 'aaronjohnsonis@gmail.com', subject: 'Tryout Registration', from: @candidate.contact1_email)
   end
 
   def registration_confirmation_email(candidate)
   	@candidate = candidate
-  	mail(to: @candidate.contact1_email, subject: 'Tryout Registration Confirmation', from: 'skeetersballclub@gmail.com')
+  	mail(to: @candidate.contact1_email, subject: 'Tryout Registration Confirmation', from: 'aaronjohnsonis@gmail.com')
   end
 
 end
