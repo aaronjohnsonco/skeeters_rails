88fb97221b242483f6be113e3ae3e33a226b8a23
Aaron Johnson <aaronjohnsonis@gmail.com>
2015-07-01 12:25:34 -0500

    configure yml

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: "587",
-    authentication: "plain",
+    #authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV['GMAIL_USERNAME'],
     password: ENV['GMAIL_PASSWORD']
