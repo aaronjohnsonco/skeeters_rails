7dcc692698237854602e5ea4dbc251839371c3f8
Aaron Johnson <aaronjohnsonis@gmail.com>
2015-07-01 11:19:53 -0500

    fix typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'skeetersbaseballclub.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address: "smtp.gmail.com",
+    address: "smtp.gmail.com",
     port: "587",
     domain: "gmail.com",
     authentication: "plain",
